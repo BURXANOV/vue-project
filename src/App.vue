be93cffea82a2e0b412673d1da23e2fb5f4d96a6
Bekbolot <burxanovbeko@gmail.com>
2024-12-12 22:07:00 +0600

    C

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MyPage from "./components/MyPage.vue";
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen bg-blue-500">
+    <h1 class="text-white text-4xl font-bold">Привет, Vue + Tailwind!</h1>
+  </div>
+  <MyPage />
+</template>
+
+<style scoped></style>
